02930fa2ed71147611258f4d54d80427c0a268a1
Vishal Suman <119152809+vishal3156@users.noreply.github.com>
2023-05-12 21:34:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
+  const [isMarco, setIsMarco] = useState(true);
+  const handleClick = () => {
+    setIsMarco(!isMarco);
+  }
   
   return (
     <div id="main">
-
-    </div>
-  )
+      {isMarco ? 
+        (<>
+          <h1 id="marco-polo">Marco</h1>
+          <button id="marco-polo-toggler" onClick={handleClick}>Polo</button>
+        </>) 
+      : (<>
+           <h1 id="marco-polo">Polo</h1>
+           <button id="marco-polo-toggler" onClick={handleClick}>Marco</button>
+        </>) 
+      }
+     </div>
+  );
 }
 
 
